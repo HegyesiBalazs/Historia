63e34c47dad70c4aa56c321c07f51148ba2dc9f3
marosvolgyimgy <72457174860@szily.hu>
2025-04-12 16:23:21 +0200

    no need

--- IImasodikvh+jegyzetek+térkép/adatbazis.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=historia", "root", "");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->exec("SET NAMES 'utf8'");
-} catch (PDOException $e) {
-    die("Adatbázis kapcsolati hiba: " . $e->getMessage());
-}
-?>
